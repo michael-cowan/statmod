935c3eb6738f8f3465d78182d18e7f45a1335d75
Michael Cowan <mcowan92@gmail.com>
2017-10-06 20:44:40 -0400

    Add files via upload

---
+++ stat.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from linear import *
+
+# generic data paths
+path_gen = 'Data\MLB_{}_stats.csv'
+
+# generic batting & fielding data
+bat = pd.read_csv(path_gen.format('batting'))
+field = pd.read_csv(path_gen.format('fielding'))
+
+
+year = bat.groupby('yearID')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+f = np.vectorize(lambda n, y: bat[(bat.G < n) & (bat.yearID == y)].count().playerID / float(len(bat[bat.yearID == y])))
+
+ns = np.arange(bat.G.max() + 1)
+years = range(bat.yearID.min(), bat.yearID.max()+1)
+
+b = bat[bat.H > 100]
+
+x1 = b.age.tolist()
+x2 = b.G.tolist()
+y = b.H.tolist()
+
+plt.plot(x1, x2, '.')
+plt.show()
+
+#sol = ols_2d(x1, x2, y)
+"""
+for y in years:
+    plt.plot(ns, 100.*f(ns, y))
+plt.xlabel('Max # of Games Played')
+plt.ylabel('% of Players in League')
+plt.show()
+"""
