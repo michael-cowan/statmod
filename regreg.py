88fda4f574091fcc1b93bde6b341643f31d1bb06
michael-cowan <mcowan92@gmail.com>
2017-10-10 19:43:45 -0400

    created test function to test regular regression function

--- regreg.py
+++ regreg.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.optimize import minimize
+import matplotlib.pyplot as plt
 
 def elastic_net(func, x, y, bguess, alpha, lam):
 
@@ -13,25 +14,34 @@ def elastic_net(func, x, y, bguess, alpha, lam):
 
     return minimize(tot_func, bguess, (alpha, lam))
 
-if __name__ == '__main__':
-    def func(x, b): return np.poly1d(b)(x)
+def polyfunc(x, b):
+    """
+        y = (b0)x^N + (b1)x^(N-1) + ... + bN
+    """
+    return np.poly1d(b)(x)
 
+def test(alpha=0.5, lam=[0.1, 0.001]):
+    """
+        Tests regularized regression fitting of points from
+        y = x^2 + 2x + 3
+    """
     x = np.linspace(0, 5, 100)
     y = np.poly1d([1, 2, 3])(x) + np.random.random(len(x))
     
     bguess = np.array([4, 4, 4])
-    
-    alpha = 0.5
-    lam = 0.01
-    
-    import matplotlib.pyplot as plt
+
     leg = []
     for lam in [0.1, 0.001]:
-        sol = elastic_net(func, x, y, bguess, alpha, lam)
-        plt.plot(x, func(x, sol.x), '.')
-        leg.append(str(lam))
+        sol = elastic_net(polyfunc, x, y, bguess, alpha, lam)
+        plt.plot(x, polyfunc(x, sol.x))
+        leg.append('Lambda = %s' % lam)
 
-    plt.plot(x, func(x, [1, 2, 3]))
+    plt.plot(x, polyfunc(x, [1, 2, 3]), '--', color='black')
     leg.append('Actual')
     plt.legend(leg)
+    title = 'Ridge Regression' if alpha == 0 else 'LASSO' if alpha == 1 else 'Elastic Net\nAlpha = %.2f' % alpha
+    plt.title(title)
     plt.show()
+
+if __name__ == '__main__':
+    test()
