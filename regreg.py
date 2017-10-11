4730fd09eaf850c271ca2c599949d5ce60ae2208
michael-cowan <mcowan92@gmail.com>
2017-10-10 20:45:34 -0400

    format fixes & removed OLS

--- regreg.py
+++ regreg.py
@@ -21,27 +21,27 @@ def polyfunc(x, b):
     """
     return np.poly1d(b)(x)
 
-def test(alpha=0.5, lam=[0.1, 0.001], showfigs=True):
+def test(alpha=0.5, lams=[0.5, 0.1, 0.001], testfuncb=[2, 3, 4], showfigs=True):
     """
         Tests regularized regression fitting of points from
         y = x^2 + 2x + 3
     """
     x = np.linspace(1, 5)
-    y = np.poly1d([1, 2, 3])(x)# + np.random.random(len(x))
+    y = np.poly1d(testfuncb)(x)# + np.random.random(len(x))
     
     bguess = np.array([4, 4, 4])
 
     fig, ax = plt.subplots(figsize=(11, 9))
     leg = []
-    for lam in [1.0, 0.5, 0.1, 0.0001]:
+    for lam in lams:
         sol = elastic_net(polyfunc, x, y, bguess, alpha, lam)
-        ax.plot(x, polyfunc(x, sol.x), alpha=0.75)
+        ax.plot(x, polyfunc(x, sol.x), alpha=0.8)
         leg.append('Lambda = %s' % lam)
 
     # solve with OLS
-    sol2 = ols.ols_sing(x, y, show=False)
-    ax.plot(x, polyfunc(x, sol2[1]))
-    leg.append('OLS Solution')
+    #sol2 = ols.ols_sing(x, y, show=False)
+    #ax.plot(x, polyfunc(x, sol2[1]))
+    #leg.append('OLS Solution')
 
     # plot the actual function
     ax.plot(x, y, '--', color='black')
@@ -49,10 +49,11 @@ def test(alpha=0.5, lam=[0.1, 0.001], showfigs=True):
     ax.legend(leg)
     title = 'Ridge Regression' if alpha == 0 else 'LASSO' if alpha == 1 else 'Elastic Net'
     fig.canvas.set_window_title(title)
-    ax.set_title('Alpha = %.2f' % alpha)
+    ax.set_title('%s\nAlpha = %.2f' % (title, alpha))
     if showfigs:
         fig.show()
     return fig, ax
 
 if __name__ == '__main__':
+    # test all ridge regression, elastic net, and LASSO
     (f1, a1), (f2, a2), (f3, a3) = [test(n) for n in [0, 0.5, 1]]
