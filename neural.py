a1b7fd48307b3e75d101b03e8fa0282ff8ae1f9a
michael-cowan <mcowan92@gmail.com>
2017-10-31 09:02:29 -0400

    completed simple neural network implementation w/ e.g.

--- neural.py
+++ neural.py
@@ -31,6 +31,9 @@ class NeuralNetwork:
         
         # network's outputs
         self.sol = self.forward()
+        
+        # change in weights per step
+        self.change = 10.
 
     def sigmoidal(self, mat):
         # activation function, [0, 1] interval
@@ -44,21 +47,59 @@ class NeuralNetwork:
         # solve forwards for outputs
         return self.sigmoidal(np.dot(self.hout(), self.w2) + self.b2)
 
+    def func(self, x=None):
+        if x:
+            h = self.sigmoidal(np.dot(x, self.w1) + self.b2)
+            return self.sigmoidal(np.dot(h, self.w2) + self.b2)
+        else:
+            return self.forward()
+
     def backprop(self):
         # calc new w2
-        dedo2 = self.sol - self.out
-        dodnet2 = self.sol * (1 - self.sol)
-        dnetdw2 = np.vstack(self.hout())
+        self.dedo2 = self.sol - self.out
+        self.dodnet2 = self.sol * (1 - self.sol)
+        self.dnetdw2 = np.vstack(self.hout())
         
         # dE / dW2 solved using chain rule
-        dedw2 = dedo2 * dodnet2 * dnetdw2
+        self.dedw2 = self.dedo2 * self.dodnet2 * self.dnetdw2
 
         # new w2 values
-        self.new_w2 = self.w2 - self.gamma * dedw2
+        self.new_w2 = self.w2 - self.gamma * self.dedw2
 
         #TODO: calc new w1 matrix
+        # dE/dOH = dE/dO * dO/dNET * newW2
+        self.dedoh = (np.dot(np.dot(self.dedo2, self.dodnet2), self.new_w2)).sum(1)
+        
+        # dOH/dNET = OH (1 - OH)
+        self.dohdnet = self.hout() * (1 - self.hout())
+        
+        # dNETH1/dW = w1
+        self.dneth1dw = self.w1
+        
+        self.dedw1 = np.dot(np.dot(self.dedoh, self.dohdnet), self.dneth1dw)
+        
+        self.new_w1 = self.w1 - self.gamma * self.dedw1
+        
+        self.sol = self.forward()
+
+        self.change = abs((self.w1 - self.new_w1)).sum() + abs((self.w2 - self.new_w2)).sum()
+
+        # update weights
+        self.w1 = self.new_w1
+        self.w2 = self.new_w2
+
+    def solve(self, e=1E-6):
+        while self.change > e:
+            self.backprop()
+        print 'Solution found'
+        
 
 if __name__ == '__main__':
     x = np.array([2.0, 0.4])
-    y = np.array([0.5, 3.2])
+    y = np.array([0.3, 0.9])
     n = NeuralNetwork(x, y)
+    n.solve()
+    print '\nModel:'
+    print n.sol
+    print '\nExpected:'
+    print y
