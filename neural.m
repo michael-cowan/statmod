18250417484704e600a443d1ac76b8740444c42b
michael-cowan <mcowan92@gmail.com>
2017-12-05 15:43:25 -0500

    changed names from COS to CRS

--- neural.m
+++ neural.m
@@ -2,6 +2,6 @@ main = csvread('Data\MLB_batting_statsML.csv', 2, 6);
 G_avg = main(:, [1, 20]).';
 H = main(:, 4).';
 % 
-% cos = importCOS(9);
-% x = cos(:, 1:end-1).';
-% y = cos(:, end).';
+% crs = importCRS(9);
+% x = crs(:, 1:end-1).';
+% y = crs(:, end).';
