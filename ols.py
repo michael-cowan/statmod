c78cbb637da7303100835361c3380445f768d1d4
michael-cowan <mcowan92@gmail.com>
2019-02-19 17:56:42 -0500

    improved Solution's func method to calculate yhat with model

--- ols.py
+++ ols.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 
 
 class Solution:
-    def __init__(self, format, b, pval, r2, mse, name, resid):
-        self.info = {'format': 'string representation of model',
+    def __init__(self, form, b, pval, r2, mse, name, resid):
+        self.info = {'form': 'string representation of model',
                      'b': 'model coefficients',
                      'pval': 'p distribution numbers',
                      'r2': 'correlation coefficient (R^2)',
@@ -17,66 +17,64 @@ class Solution:
                      'resid': 'target - output'
                      }
 
-        self.format = format
+        self.form = form
         self.b = b
         self.pval = pval
         self.r2 = r2
         self.mse = mse
         self.name = name
         self.resid = resid
-        if format:
-            self.sum = pd.DataFrame({'Format': self.format.split(', '),
-                                    'Coefficient': self.b.T.tolist(),
-                                    'PValue': self.pval.tolist()
-                                    })[['Format', 'Coefficient', 'PValue']]
+        if form:
+            self.summ = pd.DataFrame({'Format': self.form.split(', '),
+                                      'Coefficient': self.b.T.tolist(),
+                                      'PValue': self.pval.tolist()
+                                      })[['Format', 'Coefficient', 'PValue']]
         else:
-            self.sum = pd.DataFrame()
+            self.summ = pd.DataFrame()
+
+    def __calc_row__(self, row, xin):
+        n_ls = row.Format.replace('x', '').split('*')
+        sol = row.Coefficient
+        if row.Format == 'b':
+            return sol
+        for n in n_ls:
+            if '^' in n:
+                i, exp = map(int, n.split('^'))
+                sol *= xin[i - 1] ** exp
+            else:
+                sol *= xin[int(n) - 1]
+        return sol
 
     def func(self, xin):
-        if self.sum.empty:
-            print 'This regression resulted in an exception. A solution was not found'
-            return
-        if isinstance(xin, int) or isinstance(xin, float):
-            xin = [xin]
-        ans = 0
-        for i, row in self.sum.iterrows():
-            if '*' in row.Format:
-                n1, n2 = map(int, row.Format.replace('x', '').split('*'))
-                ans += row.Coefficient * xin[n1 - 1] * xin[n2 - 1]
-            elif '^' in row.Format:
-                n3, p1 = map(int, row.Format.replace('x', '').split('^'))
-                ans += row.Coefficient * (xin[n3 - 1] ** p1)
-            elif 'x' in row.Format:
-                ans += row.Coefficient * xin[int(row.Format.replace('x', '')) - 1]
-            else:
-                ans += row.Coefficient
-        return ans
+        """ Calculates model output (yhat) using xin """
+        return self.summ.apply(lambda r: self.__calc_row__(r, xin),
+                               axis=1).sum()
 
     def show(self):
         if self.name:
             print 'Name: "%s"\n' % self.name
-        print 'Format:\n%s\n' % self.format
-        print 'coefficients:\n%s\n' % ', '.join([str(i) for i in self.b]) 
+        print 'form:\n%s\n' % self.form
+        print 'coefficients:\n%s\n' % ', '.join([str(i) for i in self.b])
         print 'P Values:\n%s\n' % ', '.join([str(i) for i in self.pval])
         print 'R^2: %s' % str(self.r2)
         print '_' * 100
 
 
 def fit(x, y, order=2, intercept=True, pair_terms=False, show=True,
-        name='', return_ols=True):
+        name='', return_ols=True, force=False):
     # make sure xi and y are of type np.ndarray
     for z in [x, y]:
         if not (isinstance(z, list) or isinstance(z, np.ndarray)):
             raise TypeError("Inputs must be a list or array")
     x = np.array(x)
     if len(x.shape) == 1:
-        return ols_sing(x, y, order, intercept, show, name, return_ols)
+        return ols_sing(x, y, order, intercept, show, name, return_ols, force)
     else:
         return ols_multi(x, y, order, intercept, pair_terms,
                          show, name, return_ols)
 
 
-def ols(x, y, format='', show=True, name=''):
+def ols(x, y, form='', show=True, name=''):
     # average y from data
     y_avg = y.mean()
 
@@ -90,7 +88,10 @@ def ols(x, y, format='', show=True, name=''):
     except Exception as e:
         if show:
             print e
-        return Solution('', np.array([]), np.array([]), 0, 0, str(e), np.array([]))
+        return Solution('', np.array([]),
+                        np.array([]), 0,
+                        0, str(e),
+                        np.array([]))
 
     # estimates
     y_est = np.array([np.dot(x[i, :], b) for i in xrange(len(x[:, 0]))])
@@ -114,9 +115,10 @@ def ols(x, y, format='', show=True, name=''):
     # Residual mean square (Variance)
     var = resid_sos / resid_dof
 
-    # standard error (std deviation) of each coefficient = ((VCV matrix diagonal) * variance)^0.5
+    # standard error (std deviation) of each
+    # coefficient = ((VCV matrix diagonal) * variance)^0.5
     std_err = (vcv.diagonal() * var)**0.5
-    
+
     # abs(coefficient / standard error) = T-distribution statistic
     t_stat = abs(np.hstack(b) / std_err)
 
@@ -131,23 +133,24 @@ def ols(x, y, format='', show=True, name=''):
     reg_sos = tot_sos - resid_sos
 
     # sum of squares (all)
-    #sos = np.array([tot_sos, resid_sos, reg_sos])
+    # sos = np.array([tot_sos, resid_sos, reg_sos])
 
     # mean squares: (sum of sq.) / (DOF)
-    #mean_sq = sos / dof
+    # #mean_sq = sos / dof
 
     # F: found by dividing two variances
-    #resid_mean_sq = mean_sq[1]
-    #reg_mean_sq = mean_sq[2]
+    # #resid_mean_sq = mean_sq[1]
+    # #reg_mean_sq = mean_sq[2]
 
-    # F distribution, excel func: FDIST(mean_sq_REG / mean_sq_RESID, DOF_REG, DOF_RESID)
-    #prob_f = 1 - stats.f.cdf(reg_mean_sq / resid_mean_sq, reg_dof, resid_dof)
+    # F distribution, excel func:
+    # FDIST(mean_sq_REG / mean_sq_RESID, DOF_REG, DOF_RESID)
+    # prob_f = 1 - stats.f.cdf(reg_mean_sq / resid_mean_sq, reg_dof, resid_dof)
 
     # correlation coefficient, R^2 = (tot_sos - resid_sos) / (tot_sos)
     r2 = reg_sos / tot_sos
 
     # Reverse format, b, & pval such that its order matches np.poly1d
-    format = ', '.join(format.replace(',', '').split()[::-1])
+    form = ', '.join(form.replace(',', '').split()[::-1])
     if len(b.shape) == 1:
         b = b[::-1]
     else:
@@ -156,36 +159,38 @@ def ols(x, y, format='', show=True, name=''):
 
     if show:
         print 'Name:\n%s\n' % name
-        print 'Format:\n%s\n' % format
-        print 'coefficients:\n%s\n' % ', '.join([str(i) for i in b]) 
+        print 'Format:\n%s\n' % form
+        print 'coefficients:\n%s\n' % ', '.join([str(i) for i in b])
         print 'P Values:\n%s\n' % ', '.join([str(i) for i in pval])
         print 'R^2: %s' % str(r2)
         print '_' * 100
 
-    return Solution(format, b, pval, r2, mse, name, resid)
+    return Solution(form, b, pval, r2, mse, name, resid)
 
 
 def ols_sing(x1, y, order=2, intercept=True, show=True, name='',
-             return_ols=True):
+             return_ols=True, force=False):
     assert isinstance(x1, list) or isinstance(x1, np.ndarray)
     assert isinstance(y, list) or isinstance(y, np.ndarray)
     assert len(x1) == len(y)
     assert order >= 1
-    
+
     x = np.array([[1]*len(x1), x1]).T if intercept else np.vstack(x1)
     y = np.array(y)
-    
-    format = 'b, x1' if intercept else 'x1'
-    
+
+    form = 'b, x1' if intercept else 'x1'
+
     if order > 1:
         x1 = np.vstack(x1)
         for i in xrange(2, order+1):
             x = np.concatenate((x, x1**i), axis=1)
-            format += ', x1^%i' % i
+            form += ', x1^%i' % i
 
-    assert x.shape[1] < len(x)-1, "More data or a lower order is needed to complete OLS"
+    if not force:
+        assert x.shape[1] < len(x)-1, "More data or a lower order is needed " \
+                                      "to complete OLS"
 
-    return ols(x, y, format, show, name) if return_ols else (x, format)
+    return ols(x, y, form, show, name) if return_ols else (x, form)
 
 
 def ols_multi(xi, y, order=2, intercept=True, pair_terms=True, show=True,
@@ -206,17 +211,17 @@ def ols_multi(xi, y, order=2, intercept=True, pair_terms=True, show=True,
         xi = np.vstack(xi)
 
     # initialize format
-    format = ''
+    form = ''
     xmade = False
 
     # add intercept
     if intercept:
-        format += 'b '
+        form += 'b '
         x = np.vstack([1]*len(xi))
         xmade = True
 
     # add in first order inputs to format & x
-    format += ', '.join(['x' + str(i+1) for i in xrange(xi.shape[1])])
+    form += ', '.join(['x' + str(i+1) for i in xrange(xi.shape[1])])
     if xmade:
         x = np.concatenate((x, xi), axis=1)
     else:
@@ -224,44 +229,25 @@ def ols_multi(xi, y, order=2, intercept=True, pair_terms=True, show=True,
 
     # add second order terms
     if order == 2:
-        format += ', ' + ', '.join(['x' + str(j+1) + '^2' for j in xrange(xi.shape[1])])
+        form += ', ' + ', '.join(['x' + str(j+1) + '^2'
+                                  for j in xrange(xi.shape[1])])
         x = np.concatenate((x, xi**2), axis=1)
 
     # add pair terms
     if pair_terms:
         for z1 in xrange(1, xi.shape[1]):
             for z2 in xrange(z1+1, xi.shape[1]+1):
-                format += ', x%i*x%i' %(z1, z2)
+                form += ', x%i*x%i' % (z1, z2)
                 x = np.concatenate((x, np.vstack(x[:, z1] * x[:, z2])), axis=1)
 
-    #assert x.shape[1] < len(x)-1, "More data or a lower order is needed to complete OLS"
-    
-    return ols(x, y, format, show, name) if return_ols else (x, format)
+    return ols(x, y, form, show, name) if return_ols else (x, form)
 
 if __name__ == '__main__':
+    import time
     x = np.random.random([500, 10])
     y = x.sum(axis=1)
-    
+
     sol = ols_multi(x, y, 2)
-    """
-    order = 2
-    intercept = True
-    
-    x = np.array([1, 2, 3, 4, 5])
-    y = np.poly1d([-0.5, 4, 12])(x)
-    
-    sol = ols_sing(x, y, order, intercept, show=True)
 
-    coef = sol[1]
-    st = 0 if intercept else 1
-
-    f = np.poly1d(coef)
-    
-    xn = np.linspace(0, x.max())
-    yn = f(xn)
-    
-    plt.plot(x, y, 'x')
-    plt.plot(xn, yn)
-    plt.title('R2 = %0.4f' % sol[-1])
-    plt.show()
-    """
+    z = [23, 22, 2, 34, 6, 322, 4, 65, 345, 432]
+    ans = sol.func(z)
