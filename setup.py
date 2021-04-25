6110d32540fc33bbbaa8926b13212122e2b29782
michael-cowan <mcowan92@gmail.com>
2021-04-25 19:57:03 -0400

    changed numpy to NumPy

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(name='statmod',
                  version=__version__,
                  author='Michael Cowan',
                  url='https://www.github.com/michael-cowan/statmod',
-                 description="statistical models written in numpy",
+                 description="statistical models written in NumPy",
                  packages=setuptools.find_packages(),
                  python_requires='>=3',
                  install_requires=['numpy'])
