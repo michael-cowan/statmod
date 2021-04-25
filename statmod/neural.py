118397ee1e3531b20d83cc56701a03ac5e35a5a7
michael-cowan <mcowan92@gmail.com>
2021-04-25 19:24:29 -0400

    reworked NN to use batches during backpropagation

--- statmod/neural.py
+++ statmod/neural.py
@@ -1,17 +1,28 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+Example Artifical Neural Network
+- constrained to 1 input, 1 hidden layer, and 1 output
+- can adjust number of nodes in hidden layer
+- uses batch backpropagation to train
+"""
 
 
 class NeuralNetwork:
-    def __init__(self, inputs, outputs, neurons=2):
-        # rinput matrix
-        self.x = np.array(inputs)
-        if len(self.x.shape) == 1:
-            self.x = self.x.reshape(1, self.x.size)
+    def __init__(self, inputs, outputs, neurons=2, activation='sigmoid'):
+        # input matrix
+        self.x = np.vstack(inputs)
 
         # target values
-        self.yact = np.array(outputs)
-        if len(self.yact.shape) == 1:
-            self.yact = self.yact.reshape(1, self.yact.size)
+        self.yact = np.vstack(outputs)
+
+        # predicted values
+        self.y = None
+
+        # define activation function and differential func
+        self.activation = getattr(self, activation)
+        self.diff_activation = getattr(self, 'diff_' + activation)
 
         # number of inputs
         self.nin = self.x.shape[1]
@@ -23,145 +34,117 @@ class NeuralNetwork:
         self.neurons = neurons
 
         # weights from inputs to hidden neuron layer
-        self.w1 = np.random.random([self.nin, neurons])
-        # self.w1 = np.array([[0.15, 0.25], [0.2, 0.3]])
+        self.w1 = 2 * np.random.random([self.nin, neurons]) - 1
 
         # bias term between inputs and hidden neuron layer
         self.b1 = np.random.random()
-        # self.b1 = 0.35
 
         # weights from hidden layer to outputs
-        self.w2 = np.random.random([neurons, self.nout])
-        # self.w2 = np.array([[0.40, 0.50], [0.45, 0.55]])
+        self.w2 = 2 * np.random.random([neurons, self.nout]) - 1
 
         # bias term between hidden layer and outputs
         self.b2 = np.random.random()
-        # self.b2 = 0.60
-
-        # accounts for step change within gradient descent
-        # optimization approach
-        self.gamma = 0.5
 
         # calc network's outputs
-        # self.forward()
+        self.forward()
 
-        # change in weights per step
-        self.change = 10.
+    def __call__(self, x):
+        h = self.activation(np.dot(np.vstack(x), self.w1) + self.b2)
+        return self.activation(np.dot(h, self.w2) + self.b2)
 
-    def sigmoidal(self, mat):
+    def relu(self, arr):
+        return arr * (arr > 0).astype(float)
+
+    def diff_relu(self, arr):
+        return (arr > 0).astype(float)
+
+    def sigmoid(self, arr):
         # activation function, [0, 1] interval
-        return 1 / (1 + np.exp(-mat))
+        return 1 / (1 + np.exp(-arr))
 
-    def diff_sigmoidal(self, mat):
+    def diff_sigmoid(self, arr):
         # derivative of activation function
-        return np.exp(-mat) / (1 + np.exp(-mat))**2
+        return np.exp(-arr) / (1 + np.exp(-arr))**2
 
     def forward(self):
         # solve forwards for outputs
         self.h = np.dot(self.x, self.w1) + self.b1
-        self.hout = self.sigmoidal(self.h)
+        self.hout = self.activation(self.h)
         self.net = np.dot(self.hout, self.w2) + self.b2
-        self.y = self.sigmoidal(self.net)
+        self.y = self.activation(self.net)
+        self.e = (0.5 * (self.y - self.yact)**2).mean()
 
-    def func(self, x=None):
-        if x:
-            h = self.sigmoidal(np.dot(x, self.w1) + self.b2)
-            return self.sigmoidal(np.dot(h, self.w2) + self.b2)
-        else:
-            self.forward()
-            return self.y
-
-    def backprop(self):
-        # solve for dedw2
-        self.dedy = self.y - self.yact
-
-        self.dydnet = self.y * (1 - self.y)
-
-        self.dednet = (self.dedy * self.dydnet)
-
+    def backprop(self, learning_rate):
+        # solve for (de / dw2) = (de / dy) (dy / dnet) (dnet / dw2)
+        self.dedy = (self.y - self.yact)
+        self.dydnet = self.diff_activation(self.net)  # self.y * (1 - self.y)        
         self.dnetdw2 = self.hout
+        self.dedw2 = np.matmul(self.dedy * self.dydnet.T, self.dnetdw2)
 
-<<<<<<< HEAD
-        self.dedw2 = sum([i * np.vstack(j) for i, j in zip(self.dednet,
-                                                           self.dnetdw2)])
-=======
-        self.dedw2 = sum([i * np.vstack(j) for i,j in zip(self.dednet, self.dnetdw2)])
->>>>>>> 8e50bdfe234090bb8fd7a7a82eef8d635d61f606
+        # solve for (de/dw1) = (de/dy) (dy / dw1)
+        self.dhdw1 = self.x
+        self.dnetdw1 = self.diff_activation(self.h) * self.dhdw1 * self.w2.T
+        self.dydw1 = self.diff_activation(self.dnetdw1)
+        self.dedw1 = self.dedy * self.dydw1
 
-        # solve for dedw1
-        self.dedw1 = np.zeros(self.w1.shape)
-        self.dnetdhout = self.w2
-        self.dnetdw1 = self.x
-        self.dhoutdh = self.hout * (1 - self.hout)
-        for i in xrange(len(self.x)):
-            self.dedh = self.dednet[i, :] * self.dnetdhout
-
-            self.dedhout = n.dedh.sum(1)
-            self.dedw1 += np.matmul((self.dedhout * self.dhoutdh[i, :]).reshape(1, self.neurons), self.dnetdw1[i, :])
-            # self.dedw1 = sum([np.matmul(np.vstack(self.dedhout * i), j) for i,j in zip(self.dhoutdh, self.dnetdw1)])
-
-        self.delta_w1 = - self.gamma * self.dedw1
-        self.delta_w2 = - self.gamma * self.dedw2
-
-        self.change = abs(self.delta_w1).sum() + abs(self.delta_w2).sum()
+        # average the (de / dw) values across the batch
+        self.delta_w1 = self.dedw1.mean(0, keepdims=True)
+        self.delta_w2 = self.dedw2.T.mean(1, keepdims=True)
 
         # update weights
-        self.w1 += self.delta_w1
-        self.w2 += self.delta_w2
+        self.w1 -= learning_rate * self.delta_w1
+        self.w2 -= learning_rate * self.delta_w2
 
-        # calc new outputs
+    def train(self, epochs=100, batch_size=16, learning_rate=0.01):
+        X = self.x.copy()
+        yact = self.yact.copy()
+        res = np.zeros((epochs, 2))
+        res[:, 0] = range(epochs)
+        for epoch in range(epochs):
+            for j in range(0, X.shape[0], batch_size):
+                self.x = X[j: j + batch_size, :]
+                self.yact = yact[j: j + batch_size, :]
+                self.forward()
+                self.backprop(learning_rate)
+            res[epoch, 1] = self.e
+            print(f'Epoch {epoch + 1:05d}: MSE = {self.e:.5f}')
+        self.x = X
+        self.yact = yact
         self.forward()
-
-<<<<<<< HEAD
-    def train(self, e=1E-6, max_iter=10000):
-        i = 0
-        for i in xrange(max_iter):
-            self.backprop()
-            if self.change > e:
-                print('Training converged')
-                break
-        else:
-            print('Did not converge.')
+        return res
 
 
 if __name__ == '__main__':
-    x = np.array([[0.05, 0.10]])
-    # x = np.vstack(range(1, 5))
-    x.sort()
-    # yact = x ** 2
-    yact = np.array([[0.01, 0.99]])
-    ymax = float(yact.max())
-    y = yact   # / ymax
-    n = NeuralNetwork(x, y)
-    n.train()
-    if 1:
-        print '\nModel:'
-        print n.y
-        print '\nExpected:'
-        print n.yact
-=======
-    def solve(self, e=1E-6):
-        while self.change > e:
-            self.backprop()
-        print('Solution found')
+    np.random.seed(15224)
 
-if __name__ == '__main__':
-    x = np.array([0.05, 0.10, 0.15, 0.2, 0.25, 0.3, 0.35])
-    x = np.vstack(x)
-    yact = 4 - 2 * np.cos(x)
-    ymax = float(yact.max())
-    y = yact
-    n = NeuralNetwork(x, y)
-    # n.solve()
-    if 0:
-        print('\nModel:')
-        print(n.y)
-        print('\nExpected:')
-        print(n.yact)
->>>>>>> 8e50bdfe234090bb8fd7a7a82eef8d635d61f606
-        import matplotlib.pyplot as plt
-        plt.plot(n.yact, n.y, '.')
-        plt.plot(np.linspace(0, 1), np.linspace(0, 1))
-        plt.xlabel('Actual')
-        plt.ylabel('Model')
-        plt.show()
+    x = np.random.random(10_000) * 8
+
+    # fit NN to a polynomial
+    y = x ** 2 + 3 * x
+
+    # normalize outputs: [0, 1]
+    # since we're using a sigmoid activation on the output layer
+    y /= y.max()
+
+    # initialize and train neural network
+    nn = NeuralNetwork(x, y, 128)
+    results = nn.train()
+
+    # plot a learning curve
+    plt.figure(figsize=(10, 5))
+    plt.subplot(121)
+    plt.plot(results[:, 0], results[:, 1])
+    plt.xlabel('Epochs')
+    plt.ylabel('MSE')
+    plt.title('Learning Curve')
+
+    # make parity plot
+    plt.subplot(122)
+    plt.plot(nn.yact, nn.y, '.')
+    plt.plot([0, 1], [0, 1], color='k', zorder=-100)
+    plt.xlabel('Actual')
+    plt.ylabel('Model')
+    plt.title('Parity Plot')
+    plt.legend([], title=f'MSE = {nn.e:.3e}', frameon=False)
+    plt.tight_layout()
+    plt.show()
